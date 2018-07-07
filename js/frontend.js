0cad1bcd417a160295a75781a593d22bd816396e
Dan Smith <dev@dotherightthing.co.nz>
2018-07-08 01:29:50 +1200

    ES6 & JSDoc fixes

--- js/frontend.js
+++ js/frontend.js
@@ -1,14 +1,38 @@
 /**
- * Scripts for the public front-end
- *
- * PHP variables are provided in wpdtrt_contentsections_config.
- *
- * @version 	0.0.1
- * @since       0.7.0 DTRT WordPress Plugin Boilerplate Generator
+ * @file DTRT Content Sections frontend.js
+ * @summary
+ *     Front-end scripting for public pages
+ *     PHP variables are provided in `wpdtrt_contentsections_config`.
+ * @version 0.0.1
+ * @since   0.7.0 DTRT WordPress Plugin Boilerplate Generator
  */
 
-jQuery(document).ready(function($){
+/* eslint-env browser */
+/* global document, jQuery, wpdtrt_contentsections_config */
+/* eslint-disable no-unused-vars */
 
-	var config = wpdtrt_contentsections_config;
+/**
+ * @namespace wpdtrt_contentsections_ui
+ */
+const wpdtrt_contentsections_ui = {
 
+    /**
+     * Initialise front-end scripting
+     * @since 0.0.1
+     */
+    init: () => {
+        "use strict";
+
+        console.log("wpdtrt_contentsections_ui.init");
+    }
+}
+
+jQuery(document).ready( ($) => {
+
+    "use strict";
+
+	const config = wpdtrt_contentsections_config;
+    wpdtrt_contentsections_ui.init();
 });
+
+/* eslint-enable no-unused-vars */
